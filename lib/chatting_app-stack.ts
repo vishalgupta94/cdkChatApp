835ae8831167ade19488151c47435b65328fd472
Vishal <vishal.gupta@appfire.com>
2023-10-13 22:44:07 +0530

    api added

--- lib/chatting_app-stack.ts
+++ lib/chatting_app-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { WebSocketApi } from '@aws-cdk/aws-apigatewayv2-alpha'
+import * as apigwci2 from '@aws-cdk/aws-apigatewayv2-integrations-alpha'
+import { WebSocketLambdaAuthorizer } from '@aws-cdk/aws-apigatewayv2-authorizers-alpha';
+import * as path from 'path'
 export class ChattingAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const connect = new NodejsFunction(this, 'connect', {
+      handler: 'handler',
+      entry: path.join(process.cwd(),'handlers/connect/index.ts')
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ChattingAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const disconnect = new NodejsFunction(this, 'disconnect', {
+      handler: 'handler',
+      entry: path.join(process.cwd(),'handlers/disconnect/index.ts')
+    })
+
+    const socketApi = new WebSocketApi(this, 'WebSocketAPI', {
+      connectRouteOptions: {
+        integration: new apigwci2.WebSocketLambdaIntegration('ConnectIntegration', connect),
+      },
+      disconnectRouteOptions: {
+        integration: new apigwci2.WebSocketLambdaIntegration('DisConnectIntegration', disconnect)
+      }
+    })
   }
 }
